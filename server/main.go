0a420cb2086f8b230d97f7b5a32f7ce9297b3b6f
king68312 <t.ksrzsr0330@gmail.com>
2025-03-13 00:33:46 +0900

    作成機能と読み込み機能の追加

--- server/main.go
+++ server/main.go
@@ -9,11 +9,6 @@ import (
   "log"
 )
 
-var A = "I am a student"
-var B = "I recommend this course"
-var C = "I use Arch linux"
-var taskItems = []string{A, B, C}
-
 func main(){
   db,err := sql.Open("sqlite3","./todo.db")
   if err != nil{
@@ -30,36 +25,66 @@ func main(){
   if err != nil{
     log.Fatal(err)
   }
-  _, err = db.Exec(`INSERT INTO todolist (task,rank) VALUES(?, ?)`,"算数の宿題", 2)
-  if err != nil{
-    log.Fatal(err)
-  }
 
-  rows, err := db.Query("SELECT id, rank,task,done FROM todolist")
-  if err != nil{
-    log.Fatal(err)
-  }
-  defer rows.Close()
- 
-for rows.Next() {
-  var id int
-  var rank int
-  var task string
-  var done bool
-  err = rows.Scan(&id, &rank, &task,&done)
-  if err != nil{
-    log.Fatal(err)
-  }
-  fmt.Printf("%d %d %s %t\n",id, rank , task, done)
-}
   router := gin.Default() 
-  router.GET("/", IndexGET)
   router.Run(":8080")
 }
 
-func IndexGET(c *gin.Context) {
-	c.String(http.StatusOK, "Hello world!")
+//タスクの一覧取得
+func getTasks() ([]map[string]interface{}, error) {
+	rows, err := db.Query("SELECT id, rank,task,done FROM todolist")//データベースからデータの取得
+  if err != nil{
+    log.Fatal(err)
+  }
+  defer rows.Close()//処理を遅延させ、Closeを確実に呼び出される
+
+  var tasks []map[string]interface{}//map型のtasksを作成
+  for rows.Next() {//rows.Next()でデータがある限り繰り返す
+    var id int
+    var rank int
+    var task string
+    var done bool
+    err = rows.Scan(&id, &rank, &task, &done)
+    if err != nil{
+      log.Fatal(err)
+    }
+    tasks = append(tasks, map[string]interface{}{//appendの第一引数はスライス、第二引数は追加する要素
+      "id":    id,
+      "task":  task,
+      "rank":  rank,
+      "done":  done,
+    })
+  }
+  return tasks, nil
 }
+
+router.GET("/getTask",func(c *gin.Context) {  //タスクの一覧取得
+  tasks, err := getTasks()
+  if err != nil {
+    c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})//エラーが発生した場合に500エラーを返し、gin.HでJSONを返す
+    return
+  }
+  c.JSON(200, tasks)//正常に処理が終了した場合に200を返し、tasksを返す
+})
+
+router.POST("/addTask", func(c *gin.Context) {//タスクの追加
+  var json struct {
+    Task string `json:"task"`
+    Rank int    `json:"rank"`
+  }
+  if err := c.ShouldBindJSON(&json); err != nil {
+    c.JSON(500, gin.H{"error": err.Error()})//http.StatusInternalServerErrorは500エラー
+  }
+  err := createTask(json.Task, json,Rank)
+  if err != nil {
+    c.JSOn(500, gin.H{"error": "Invalid input"})
+    return
+  }
+  c.JSON(201, gin.H{"message": "Task created"})//http.StatusCreatedは201
+})
+
+
+
 
 func helloUser(writer http.ResponseWriter, request *http.Request){
   var greet = "Hello!"
